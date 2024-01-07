288fe29f9596922803efa5c888e91c67589d5de3
adawg2010 <jcebersole1@gmail.com>
2024-01-07 21:27:20 +0900

    style change

--- slimsopening.js
+++ slimsopening.js
@@ -4,3 +4,4 @@ button.addEventListener('click', (event)=>{
  button.textContent = `Click count: ${event.detail}`;
   
 })
+
---
+++ sofiansopening.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button')
+
+button.addEventListener('click', (event)=>{
+ button.textContent = `Click count: ${event.detail}`;
+  
+})
+
