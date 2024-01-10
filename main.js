638b50aaae20ddf2aaa6da4154ea86c2fcc5e70b
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2024-01-10 09:29:10 +0900

    characterPath

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+
+const character = {
+    characterChoice: function(){
+        return this.choiceA + ' ' + this.choiceB +' has been chosen.'; 
+    }
+}
+
+
+
 const choice1 = 'Slim\'s choice ';
 
 const choice2= 'Sofian\'s choice';
+
+if ( chosen ==choice1 ){
+    return characterPath1
+}else {
+    return characterPath2
+};
 
 
 
